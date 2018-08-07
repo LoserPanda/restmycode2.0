a81798af7ce38f2edacb9b6e7f03dc04416b57cb
LoserPanda <loserpanda82@gmail.com>
2018-08-07 21:03:04 +0300

    almost working tags system

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -1,13 +1,11 @@
 var tagArray = [];
 console.log("mono");
+
 function addTagToArray() {
-    // console.log("helouuu");
-    // tagArray.push($('#helptagsid').val());
-    // $('#helptagsid').empty();
-    // console.log(tagArray);
     let tagSuspection = $('#helptagsid').val().toString();
-    if (tagSuspection.substring(tagSuspection.length - 1) === " ") {
-        tagArray.push(tagSuspection.substring(0, tagSuspection.length - 1));
-    }
+    tagArray.push(tagSuspection);
+    $('#helptagsid').empty();
     console.log(tagArray);
+    $('<li></li>').text(tagSuspection).appendTo('#helptagslistid');
+    $('#tagsid').val(tagArray);
 }
