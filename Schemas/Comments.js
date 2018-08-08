2f66cc7030025ae506aba96d7b818d7b49449834
LoserPanda <loserpanda82@gmail.com>
2018-08-08 14:45:11 +0300

    Comment Schema refactored

--- Schemas/Comments.js
+++ Schemas/Comments.js
@@ -1,22 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var Comments = new Schema({
-    userId: {type: Schema.Types.ObjectId, ref: 'User'},
-    author: String,
-    comment: {
-        type: [{
-            author: {type: Schema.Types.ObjectId, ref: 'User'},
-            comment: String
-        }]
-    },
+var Comment = new Schema({
+    dataId: {type: Schema.Types.ObjectId, ref: 'Data'},
+    authorId: {type: Schema.Types.ObjectId, ref: 'User'},
+    comment: String,
     date: {
         type: Date,
         default: Date.now
     }
 },{
-    collection: 'comments'
+    collection: 'comment'
 });
 
-module.exports = mongoose.model('Comments', Comments);
+module.exports = mongoose.model('Comment', Comment);
 
--- Schemas/Data.js
+++ Schemas/Data.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var Data = new Schema({
-    userId: {type: Schema.Types.ObjectId, ref: 'User'},
     title: String,
     descript: String,
     lang: String,
@@ -13,13 +12,7 @@ var Data = new Schema({
     tags: [String],
     score: Number,
     code: String,
-    author: String,
-    comments: {
-        type: [{
-            author: {type: Schema.Types.ObjectId, ref: 'User'},
-            comment: {type: Schema.Types.ObjectId, ref: 'Comment'}
-        }],
-    }
+    author: String
 },{
     collection: 'data'
 });
