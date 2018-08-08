67ac64f0c54ea37a28496c436a7414cde84556d9
Miika <miika.huhtanen@gmail.com>
2018-08-08 20:04:25 +0300

    Tagit toimintaan vähän paremmin

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -5,11 +5,20 @@ function addTagToArray() {
     let tagSuspection = $('#helptagsid').val().toString();
     tagArray.push(tagSuspection);
     console.log(tagArray);
-    $('<li></li>').text(tagSuspection).appendTo('#helptagslistid');
+    $('<tr ><td>' + tagSuspection + '</td><td onclick=removeTagFromArray(this)><img src="/images/switch.png" ></td></tr>').addClass('deletetag').appendTo('#helptagstableid');
     $('#tagsid').val(tagArray);
     $('#helptagsid').val('');
 }
 
 function setIdToLocalStorage(id){
     localStorage.setItem("id",JSON.stringify(id));
+}
+
+function removeTagFromArray(param){
+    console.log(param.parentNode);
+    var row = param.parentNode.rowIndex;
+    param.parentNode.style.display = "none";
+    tagArray.splice(row,1);
+    $('#tagsid').val(tagArray);
+    console.log(tagArray);
 }
