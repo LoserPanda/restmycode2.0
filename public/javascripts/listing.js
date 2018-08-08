25e2916f8f045b29d716e75bdfc5448b6ac2e823
LoserPanda <loserpanda82@gmail.com>
2018-08-08 14:37:55 +0300

    date sorting

--- public/javascripts/listing.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.footernappi').click(function () {
-        $('.footernappi').text() == "HIDE CHUCK NORRIS" ?
-            $('.footernappi').text("GENERATE CHUCK NORRIS") && $('img').hide() :
-            $('.footernappi').text("HIDE CHUCK NORRIS") && $('img').show();
-    })
-});
